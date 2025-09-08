9a5f8ac1907ba4bf3845c30b8f7c3fdd6e1c9994
Dilshi Wijesinghe <dilshipiyumika@gmail.com>
2025-09-08 16:12:32 +0500

    Project inside testing

--- tests/project_inside.spec.ts
+++ tests/project_inside.spec.ts
@@ -1,10 +1,29 @@
 import { test } from '@playwright/test';
 import { ProjectsInside } from '../pages/project_inside';
 
-test.describe('Project inside', () => {
-    test('Verify open a project', async ({ page }) => {
+// test.describe('Project inside', () => {
+//     test('Verify open a project', async ({ page }) => {
+//         const project = new ProjectsInside(page);
+//         await project. openProject();
+//     });
+
+// }); 
+
+test.describe('Create project template', () => {
+    test('Verify create project template', async ({ page }) => {
         const project = new ProjectsInside(page);
-        await project. openProject();
+        await project. saveAsProjectTemplate();
     });
 
 }); 
+
+test.describe('Create project template', () => {
+    test('Verify create project template', async ({ page }) => {
+        const project = new ProjectsInside(page);
+        await project.changeProjectDetails();
+    });
+
+}); 
+
+
+//npx playwright test
