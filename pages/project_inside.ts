f02c00f873f4031098176c55406ffa8b1f6be34a
Dilshi Wijesinghe <dilshipiyumika@gmail.com>
2025-09-19 16:16:34 +0500

    project inside test

--- pages/project_inside.ts
+++ pages/project_inside.ts
@@ -57,15 +57,33 @@ export class ProjectsInside {
   }
 
   async deleteProject() {
-    await this.login();
-    await this.page.goto('https://react.worklenz.com/projects');
-    await this.page.click(`td[class='ant-table-cell ant-table-column-sort ant-table-cell-row-hover'] div[class='flex items-center']`);
-    
+    await this.openProject();
+    await this.page.click('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > main:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(3)');
+    await this.page.click("button[class='ant-btn css-1o7vu8l ant-btn-dashed ant-btn-dangerous ant-btn-color-dangerous ant-btn-variant-dashed'] span");
+    await this.page.waitForTimeout(2000); // Wait for the action to complete
+    await this.page.click("body > div:nth-child(15) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > button:nth-child(2)");
+
   }
 
+  async subscribeProject() {
+    await this.openProject();
+    await this.page.click(".ant-btn.css-1o7vu8l.ant-btn-round.ant-btn-default.ant-btn-color-default.ant-btn-variant-outlined");
+    await this.page.waitForTimeout(2000);
+  }
   
+  async unsubscribeProject() {
+    await this.openProject();
+    await this.page.click("body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > main:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(4) > span:nth-child(2)");
+    await this.page.waitForTimeout(2000);
+  }
 
+  async addTeamMember() {
+    await this.openProject();
+    await this.page.click("body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > main:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(5) > span:nth-child(2)");
+    await this.page.fill("#memberName", 'abcd@test.com');
+    await this.page.click("body > div:nth-child(20) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)");
 
+  }
 
   
 }
--- tests/project_inside.spec.ts
+++ tests/project_inside.spec.ts
@@ -17,13 +17,41 @@ test.describe('Create project template', () => {
 
 }); 
 
-test.describe('Create project template', () => {
-    test('Verify create project template', async ({ page }) => {
+test.describe('Change project details', () => {
+    test('Verify changing the project details', async ({ page }) => {
         const project = new ProjectsInside(page);
         await project.changeProjectDetails();
     });
 
 }); 
+
+test.describe('Delete project', () => {
+    test('Verify delete a project', async ({ page }) => {
+        const project = new ProjectsInside(page);
+        await project.deleteProject();
+    });
+});
+
+test.describe('Subscribe a project', () => {
+    test('Verify subscribe a project', async ({ page }) => {
+        const project = new ProjectsInside(page);
+        await project.subscribeProject();
+    });
+});
+
+test.describe('Unsubscribe project', () => {
+    test('Verify unsubscribe a project', async ({ page }) => {
+        const project = new ProjectsInside(page);
+        await project.unsubscribeProject();
+    });
+});
+
+test.describe('Invite members', () => {
+    test('Verify invite members to the project', async ({ page }) => {
+        const project = new ProjectsInside(page);
+        await project.addTeamMember();
+    });
+});
 
 
 //npx playwright test
